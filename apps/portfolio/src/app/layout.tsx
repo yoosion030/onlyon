40211ef270f34feee53e693280b378b96af291f9
yoosion030 <yoosioff@gmail.com>
2025-02-21 10:33:57 +0900

    feat(ui): 공통 컴포넌트 Footer 개발
    - primary 색상 조정
    - primary-linear color 추가
    - footer 컴포넌트 개발 및 각 layout에 추가
    
    resolves #3
    
    Signed-off-by: yoosion030 <yoosioff@gmail.com>

--- apps/portfolio/src/app/layout.tsx
+++ apps/portfolio/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./global.css";
 import "@repo/ui/styles.css";
+import { Footer } from "@repo/ui";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,7 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
